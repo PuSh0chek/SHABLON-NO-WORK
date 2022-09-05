f8e2e1112d58e7b9b7fb70eed916df4c7a410cdf
Ганин Александр Владимирович <87208475+PuSh0chek@users.noreply.github.com>
2022-09-05 16:09:21 +0300

    4.1 new post

--- build/js/main.js
+++ build/js/main.js
@@ -8,6 +8,7 @@ const buttonNew = document.querySelector('.button__new');
 let count = 1;
 let start = 0;
 let end = 10;
+const newPostId = end - 1;
 
 const makeTag = function (tagName, tagClass, placeAdd) {
   let tagContent = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : '';
@@ -37,11 +38,10 @@ const getData = async () => {
   const arrayOfPosts = responseData.map(element => element);
   arrayOfPosts.slice(start, end).forEach(element => {
     createPost(element);
-  });
-  const buttonClip = document.querySelectorAll('.button__clip');
-  buttonClip.addEventListener('click', () => {
-    console.log('data');
-  });
+  }); // const buttonClip = document.querySelectorAll('.button__clip');
+  // buttonClip.addEventListener('click', () => {
+  //   console.log('data');
+  // });
 };
 
 getData();
@@ -64,4 +64,12 @@ buttonNext.addEventListener('click', () => {
   start += 10;
   end += 10;
 });
-buttonNew.addEventListener('click', () => {});
+buttonNew.addEventListener('click', () => {
+  createPost(newPostId); // const li = makeTag('li', 'posts__element', postsList);
+  // makeTag('span', 'posts__id', li, id);
+  // makeTag('h3', 'posts__title', li, title);
+  // makeTag('p', 'posts__text', li, body);
+  // makeTag('button', 'button__clip', li);
+
+  console.log('mya');
+});
--- source/js/main.js
+++ source/js/main.js
@@ -7,6 +7,7 @@ const buttonNew = document.querySelector('.button__new');
 let count = 1;
 let start = 0;
 let end = 10;
+const newPostId = end - 1;
 
 const makeTag = (tagName, tagClass, placeAdd, tagContent = '') => {
   const element = document.createElement(tagName);
@@ -31,10 +32,11 @@ const getData = async () => {
   arrayOfPosts.slice(start, end).forEach((element) => {
     createPost(element);
   });
-  const buttonClip = document.querySelectorAll('.button__clip');
-  buttonClip.addEventListener('click', () => {
-    console.log('data');
-  });
+  // const buttonClip = document.querySelectorAll('.button__clip');
+  // buttonClip.addEventListener('click', () => {
+  //   console.log('data');
+  // });
+
 };
 
 getData();
@@ -60,5 +62,12 @@ buttonNext.addEventListener('click', () => {
 });
 
 buttonNew.addEventListener('click', () => {
-  
-})
+  createPost(newPostId);
+
+  // const li = makeTag('li', 'posts__element', postsList);
+  // makeTag('span', 'posts__id', li, id);
+  // makeTag('h3', 'posts__title', li, title);
+  // makeTag('p', 'posts__text', li, body);
+  // makeTag('button', 'button__clip', li);
+  console.log('mya');
+});

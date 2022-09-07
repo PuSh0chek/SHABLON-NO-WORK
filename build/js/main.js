148754fa8c1f640e888b5e90eecddbfb3906d196
Ганин Александр Владимирович <87208475+PuSh0chek@users.noreply.github.com>
2022-09-07 16:14:18 +0300

    4.3 коррекция

--- build/js/main.js
+++ build/js/main.js
@@ -2,9 +2,10 @@
 
 const postsContainer = document.querySelector('.posts');
 const postsList = document.querySelector('.posts__list');
-const buttonPrev = document.querySelector('.button__prev');
-const buttonNext = document.querySelector('.button__next');
-const buttonNew = document.querySelector('.button__new');
+const buttonPrev = document.querySelector('.posts__button-prev');
+const buttonNext = document.querySelector('.posts__button-next');
+const inputTitleInForm = document.querySelector('.posts__input-title');
+const inputContentInForm = document.querySelector('.posts__input-body');
 const buttonAddPost = document.querySelector('.posts__button-add');
 const buttonCleaningForm = document.querySelector('.posts__cleaning-form');
 let count = 1;
@@ -46,7 +47,6 @@ const getData = async () => {
     buttonsClip.addEventListener('click', () => {
       const parent = buttonsClip.parentElement;
       parent.remove();
-      console.log(buttonsClip.parentElement);
     });
   }
 };
@@ -57,6 +57,8 @@ buttonPrev.addEventListener('click', () => {
   getData();
 
   if (count === 1) {
+    start = 0;
+    end = 10;
     count = 1;
     buttonPrev.disabled = true;
   } else {
@@ -69,11 +71,21 @@ buttonPrev.addEventListener('click', () => {
 buttonNext.addEventListener('click', () => {
   postsList.innerHTML = '';
   getData();
-
-  if (count !== 1) {
-    count++;
-    start += 10;
-    end += 10;
-    buttonPrev.disabled = false;
-  } else {}
+  count++;
+  start += 10;
+  end += 10;
+  buttonPrev.disabled = false;
+});
+buttonAddPost.addEventListener('click', () => {
+  const title = inputTitleInForm.value;
+  const content = inputContentInForm.value;
+  makeTag('li', 'posts__element', postsList);
+  makeTag('span', 'posts__id', li, id);
+  makeTag('h3', 'posts__title', li, title);
+  makeTag('p', 'posts__text', li, content);
+  makeTag('button', 'button__clip', li);
+});
+buttonCleaningForm.addEventListener('click', () => {
+  inputTitleInForm.value = '';
+  inputContentInForm.value = '';
 });
--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,9 @@
 const postsContainer = document.querySelector('.posts');
 const postsList = document.querySelector('.posts__list');
-const buttonPrev = document.querySelector('.button__prev');
-const buttonNext = document.querySelector('.button__next');
-const buttonNew = document.querySelector('.button__new');
+const buttonPrev = document.querySelector('.posts__button-prev');
+const buttonNext = document.querySelector('.posts__button-next');
+const inputTitleInForm = document.querySelector('.posts__input-title');
+const inputContentInForm = document.querySelector('.posts__input-body');
 const buttonAddPost = document.querySelector('.posts__button-add');
 const buttonCleaningForm = document.querySelector('.posts__cleaning-form');
 
@@ -10,7 +11,6 @@ let count = 1;
 let start = 0;
 let end = 10;
 const newPostId = end - 1;
-let countPage = 
 
 const makeTag = (tagName, tagClass, placeAdd, tagContent = '') => {
   const element = document.createElement(tagName);
@@ -40,7 +40,6 @@ const getData = async () => {
     buttonsClip.addEventListener('click', () => {
       const parent = buttonsClip.parentElement;
       parent.remove();
-      console.log(buttonsClip.parentElement);
     });
   }
 
@@ -52,6 +51,8 @@ buttonPrev.addEventListener('click', () => {
   postsList.innerHTML = '';
   getData();
   if (count === 1) {
+    start = 0;
+    end = 10;
     count = 1;
     buttonPrev.disabled = true;
   } else {
@@ -65,12 +66,23 @@ buttonPrev.addEventListener('click', () => {
 buttonNext.addEventListener('click', () => {
   postsList.innerHTML = '';
   getData();
-  if (count !== 1) {
-    count++;
-    start += 10;
-    end += 10;
-    buttonPrev.disabled = false;
-  } else {
+  count++;
+  start += 10;
+  end += 10;
+  buttonPrev.disabled = false;
+});
 
-  }
+buttonAddPost.addEventListener('click', () => {
+  const title = inputTitleInForm.value;
+  const content = inputContentInForm.value;
+  makeTag('li', 'posts__element', postsList);
+  makeTag('span', 'posts__id', li, id);
+  makeTag('h3', 'posts__title', li, title);
+  makeTag('p', 'posts__text', li, content);
+  makeTag('button', 'button__clip', li);
+})
+
+buttonCleaningForm.addEventListener('click', () => {
+  inputTitleInForm.value = '';
+  inputContentInForm.value = '';
 });
